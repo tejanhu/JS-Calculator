5500ecc90c5edda94889c042f0a4d6f33733563f
Zy <zy@Husseins-MacBook-Air.local>
2023-05-06 09:03:58 +0100

    Display and operate working with some issues

--- app.js
+++ app.js
@@ -1,5 +1,79 @@
 var result;
-var display_screen = document.getElementById("display");
+var display_first_res = document.querySelector(".first-res");
+var display_present_res = document.querySelector(".present-res");
+var numBtns = document.querySelectorAll(".digit");
+var equalsBtn = document.querySelector("#equals");
+var operatorBtns = document.querySelectorAll(".operator");
+var first_num = 0;
+var present_num = null;
+var operator = null;
+var calculation = 0;
+
+numBtns.forEach(btn => {
+  btn.addEventListener('click', (e) => {
+    retrieveDigit(e.target.textContent);
+  } );
+});
+
+function retrieveDigit(num){
+    present_num += parseFloat(num);
+    present_num = parseFloat(present_num);
+    display_first_res.textContent +=  " " + present_num;
+    display_first_res.style.color = "white";
+}
+
+
+// function handleClick() {
+//   first_num = this.textContent;
+//   display_screen.textContent += first_num;
+//   display_screen.style.color = "white";
+//   return first_num;
+// }
+
+
+operatorBtns.forEach(btn => {
+    btn.addEventListener('click', (e) => {
+        retrieveOperator(e.target.textContent);
+    } );
+  });
+
+function retrieveOperator(op){
+    // If no operator has been used, that means we're still dealing with the first number
+    if(operator == null){
+        first_num = present_num;
+        console.log(first_num);
+    } else if(present_num != null){ // Otherwise it means we're dealing with the next number
+        first_num = operate(operator, first_num, present_num);
+        console.log(first_num);
+    }
+      
+      display_first_res.textContent = first_num + ' ' + op;
+      operator = op;
+      present_num = '';
+      calculation = first_num;
+      console.log(first_num);
+    //   return calculation;
+    //   display_present_res.innerHTML = "0";
+  }
+
+//   equalsBtn.addEventListener('click', (e) => {
+//         calculate(event.target.textContent);
+//     } );
+
+//   function calculate(){
+
+
+
+//     return calculation;
+//   }
+
+// function handleOperatorClick() {
+//   operator = this.textContent;
+//   display_screen.textContent += operator;
+//   display_screen.style.color = "rgb(47, 133, 47)";
+//   return operator;
+// }
+
 
 function add(first_num, second_num){
     result = first_num + second_num;
@@ -17,7 +91,12 @@ function multiply(first_num, second_num){
 }
 
 function divide(first_num, second_num){
-    result = first_num / second_num;
+    if(first_num == 0 || second_num == 0){
+        result = "Can't divide by zero."
+    }else{
+        result = first_num / second_num;
+    }
+    
     return result;
 }
 
@@ -42,8 +121,13 @@ function operate(operator, first_num, second_num){
 }
 
 // WIP - whichever buttons is clicked will display on the display 
-function populateDisplay(){
-    // if(numBtns){
-    //     display_screen.textContent = numBtns.value;
-    // }
-}
+// function populateDisplay(){
+//     numBtns.forEach(btn => {
+//         btn.addEventListener("click", function(e){
+//             display_screen.textContent += btn.innerHTML;
+//             // console.log(val);
+//         });
+//     });
+// }
+
+// populateDisplay();
